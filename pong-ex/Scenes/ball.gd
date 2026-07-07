0e883a5afe33242bba2ece98b09b64ddb37b29ba
Aditya Subramanian <adu101992@gmail.com>
2026-07-07 14:35:21 +0530

    3D pong.

--- pong-ex/Scenes/ball.gd
+++ pong-ex/Scenes/ball.gd
@@ -1,32 +1,56 @@
 extends RigidBody3D
 
-@export var forward_vector : Vector3 = -global_transform.basis.z
-@export var right_vector   : Vector3 = global_transform.basis.x
+@export var forward_vector : Vector3 = -transform.basis.z
+@export var right_vector   : Vector3 = transform.basis.x
 
 @export var fVOn : float = 0.0
 @export var rVOn : float = 0.0
 
 @export var current_direction : Vector3
+@export var force_multiplier : float = 0
+@export var dirPlayer : float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	fVOn = randf_range(0.0, 100.0)
 	rVOn = randf_range(0.0, 100.0)
+	dirPlayer = randf_range(0.0, 100.0)
 	current_direction = Vector3(0.0, 0.0, 0.0)
+	force_multiplier = 5.0
 
 	if (fVOn > 33.0):
-		current_direction += forward_vector
-	if (rVOn < 33.0) && (current_direction.is_zero_approx() == false):
-		current_direction += right_vector
+		current_direction += roundf((50.0 - dirPlayer)/50.0) * forward_vector
+	if (rVOn < 33.0):
+		current_direction += roundf((50.0 - dirPlayer)/50.0) * right_vector
+
+	apply_impulse(current_direction * force_multiplier)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if absf(position.z) > 21.0:
+		position.x = 0.0
+		position.z = 0.0
+		linear_velocity.x = 0.0
+		linear_velocity.y = 0.0
+		linear_velocity.z = 0.0
+		apply_impulse(current_direction * force_multiplier)
 
 func _physics_process(_delta: float) -> void:
-	apply_force(current_direction)
-
+	pass
 
 func _on_body_entered(_body: Node) -> void:
-	#current_direction = current_direction.bounce(_body.get_normal())
+	#print("Collided with: ", _body.owner.name)
 	pass
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	if state.get_contact_count() > 0:
+		var contact_collider = state.get_contact_collider_object(0) as Node3D
+		if contact_collider.owner.name.contains("Ground"):
+			return
+
+		var contact_position = state.get_contact_collider_position(0)
+		var contact_normal = state.get_contact_local_normal(0)
+		print("Colliding at: ", contact_position)
+		current_direction = current_direction.bounce(contact_normal)
+		apply_impulse(current_direction * force_multiplier)
