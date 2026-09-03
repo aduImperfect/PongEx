a7340225f9068c2ff17202743caf31a7ffeaba50
Aditya Subramanian <adu101992@gmail.com>
2026-09-04 00:14:57 +0530

    Builds config.

--- pong-ex/Scenes/ball.gd
+++ pong-ex/Scenes/ball.gd
@@ -9,19 +9,34 @@ extends RigidBody3D
 @export var current_direction : Vector3
 @export var force_multiplier : float = 0
 @export var dirPlayer : float = 0.0
+@export var currDirX : bool = false
+@export var currDirZ : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fVOn = randf_range(0.0, 100.0)
-	rVOn = randf_range(0.0, 100.0)
-	dirPlayer = randf_range(0.0, 100.0)
+	fVOn = randf_range(10.0, 50.0)
+	rVOn = randf_range(10.0, 50.0)
+	dirPlayer = randf_range(20.0, 100.0)
 	current_direction = Vector3(0.0, 0.0, 0.0)
 	force_multiplier = 5.0
 
 	if (fVOn > 33.0):
 		current_direction += roundf((50.0 - dirPlayer)/50.0) * forward_vector
-	if (rVOn < 33.0):
+	elif (rVOn < 33.0):
 		current_direction += roundf((50.0 - dirPlayer)/50.0) * right_vector
+
+	if absf(current_direction.x < 0.5) && absf(current_direction.z < 0.5):
+		currDirX = bool(randi_range(0, 1))
+		currDirZ = bool(randi_range(0, 1))
+		var valX = -0.5
+		if currDirX:
+			valX = 0.5
+		var valZ = -0.5
+		if currDirZ:
+			valZ = 0.5
+		current_direction += Vector3(valX, 0.0, valZ)
+
+	current_direction.y = 0.0
 
 	apply_impulse(current_direction * force_multiplier)
 
@@ -53,4 +68,8 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 		var contact_normal = state.get_contact_local_normal(0)
 		print("Colliding at: ", contact_position)
 		current_direction = current_direction.bounce(contact_normal)
+
+		if linear_velocity.length() > 10.0:
+			return
+
 		apply_impulse(current_direction * force_multiplier)
